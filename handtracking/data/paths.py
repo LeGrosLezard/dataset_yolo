9a324995385da4840c9c6bb9b5143b9cbfb22816
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-14 14:40:28 +0100

    Add files via upload

---
+++ handtracking/data/paths.py
@@ -0,0 +1,4 @@
+path_annotation = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\annotation"
+annotations = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\annotation\{}"
+path_picture = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\img"
+pictures = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\img\{}"
