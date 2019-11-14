2abb651269ea069604a1b6422aadb99996d071e5
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-14 15:10:33 +0100

    Update paths.py

--- handtracking/data/paths.py
+++ handtracking/data/paths.py
@@ -2,3 +2,5 @@ path_annotation = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\an
 annotations = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\annotation\{}"
 path_picture = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\img"
 pictures = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\img\{}"
+path_txt_annotation = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\txt_annotation"
+
