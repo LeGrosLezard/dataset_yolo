36518a096bfbe58faae2349749270d222ac973dc
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-27 18:03:03 +0100

    Delete paths.py

--- handtracking/data/paths.py
+++
@@ -1,6 +0,0 @@
-path_annotation = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\annotation"
-annotations = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\annotation\{}"
-path_picture = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\img"
-pictures = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\img\{}"
-path_txt_annotation = r"C:\Users\jeanbaptiste\Desktop\jgfdposgj\handtracking\data\txt_annotation"
-
