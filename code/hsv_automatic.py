b77541ca15599c8730387fcac48395d2e7537e1e
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-29 21:29:41 +0100

    Update hsv_automatic.py

--- code/hsv_automatic.py
+++ code/hsv_automatic.py
@@ -1,38 +1,19 @@
 import cv2
 import numpy as np
 
-from trackbarre_hsv import barre_tool
 from trackbarre_hsv import parameter_ajusted
-from trackbarre_hsv import nothing
 
 
-#detecter les mains via un hsv automatique
-
-#supression de fond
-#récupérer la chose qui bouge le plus
-#faire une espece de fonction qui dit tete // main
-#barre hsv de la chose qui bouge le plus
-#detection des mains
-
-
-def blanck_picture(img):
-    """ Create a black picture"""
-    blank_image = np.zeros((img.shape[0],img.shape[1],3), np.uint8)
-    blank_image[0:, 0:] = 0, 0, 0
-    return blank_image
 
 
 def video_lecture(video_name):
 
     video = cv2.VideoCapture(video_name)
 
-    _, frame1 = video.read()
-    _, frame2 = video.read()
+    frame1 = cv2.resize(video.read()[1], (250, 150))
+    frame2 = cv2.resize(video.read()[1], (250, 150))
 
-    frame1 = cv2.resize(frame1, (300, 200))
-    frame2 = cv2.resize(frame2, (300, 200))
-
-
+    hiear = cv2.RETR_TREE; pts = cv2.CHAIN_APPROX_NONE
 
     while True:
 
@@ -41,14 +22,13 @@ def video_lecture(video_name):
         diff = cv2.absdiff(frame1, frame2)
         mask, res = parameter_ajusted(frame1)
 
-        gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-
-        _, thresh = cv2.threshold(gray, 5, 255, cv2.THRESH_BINARY)
-        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
+        _, thresh = cv2.threshold(cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY),
+                                  5, 255, cv2.THRESH_BINARY)
+ 
+        contours, _ = cv2.findContours(thresh, hiear, pts)
 
         for contour in contours:
-            if cv2.contourArea(contour) < 100:
+            if 10 > cv2.contourArea(contour) < 100:
                 cv2.fillPoly(thresh, pts =[contour], color=(0,0,0))
 
                 kernel = np.ones((3,3), np.uint8)
@@ -61,13 +41,13 @@ def video_lecture(video_name):
                     frame1[x, y] = 0
 
 
-        l_b = np.array([0, 75, 130])
-        u_b = np.array([255, 255, 255])
-
         hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, l_b, u_b)
 
-        contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        mask = cv2.inRange(hsv, np.array([0, 75, 130]),
+                           np.array([255, 255, 255]))
+
+        contours, _ = cv2.findContours(mask, hiear, pts)
+ 
         for contour in contours:
             if cv2.contourArea(contour) > 5:
                 (x, y, w, h) = cv2.boundingRect(contour)
@@ -75,31 +55,21 @@ def video_lecture(video_name):
 
 
         cv2.imshow('Automatic HSV', copy)
-        cv2.imshow('mask HSV', mask)
 
 
         frame1 = frame2
-        _, frame2 = video.read()
-        frame2 = cv2.resize(frame2, (300, 200))
+        frame2 = cv2.resize(video.read()[1], (250, 150))
 
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
-
+        if cv2.waitKey(0) & 0xFF == ord('q'): break
 
     video.release()
     cv2.destroyAllWindows()
 
 
+
+
 if __name__ == "__main__":
 
-    video_name = "a.mp4"
+    video_name = "video/a.mp4"
 
     video_lecture(video_name)
-
-
-
-
-
-
-
-
