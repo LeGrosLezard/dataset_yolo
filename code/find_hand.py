a772a8ab448ec6dcc3806267d876c5a95d66e613
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-30 16:28:56 +0100

    Update find_hand.py

--- code/find_hand.py
+++ code/find_hand.py
@@ -10,7 +10,6 @@ def find_head(face_cap, out, detector, frame1, frame2):
     diff = cv2.absdiff(frame1, frame2)
 
     #Collect points of face
-
     points = [[face.left(), face.top(), face.right(), face.bottom()] for
               face in detector(cv2.cvtColor(face_cap, cv2.COLOR_BGR2GRAY))]
 
@@ -18,28 +17,34 @@ def find_head(face_cap, out, detector, frame1, frame2):
                           [points[0][2] + 10, points[0][1] - 50],
                           [points[0][2] + 10, points[0][3] + 10],
                           [points[0][0] - 10, points[0][3] + 10] ])
-                          
-    cv2.fillPoly(diff, pts=[contours], color=(255, 255, 255))
+
+    cv2.fillPoly(diff, pts=[contours], color=(0, 0, 0))
 
     return diff
 
 
 def find_hand(copy, diff):
 
-    mask = cv2.inRange(cv2.cvtColor(diff, cv2.COLOR_BGR2HSV),
-                        np.array([0, 32, 30]),
-                        np.array([255, 255, 255]))
+    imageYCrCb = cv2.cvtColor(diff, cv2.COLOR_BGR2YCR_CB)
+    skinRegionYCrCb = cv2.inRange(imageYCrCb, np.array([0,133,77]),
+                                            np.array([235,173,127]))
 
-    contours, _ = cv2.findContours(mask, cv2.RETR_TREE,
+    skinYCrCb = cv2.bitwise_and(diff, diff, mask = skinRegionYCrCb)
+
+    gray = cv2.cvtColor(skinYCrCb,cv2.COLOR_BGR2GRAY)
+    thresh = cv2.threshold(gray, 5, 255, 1)[1]
+
+
+    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE,
                                    cv2.CHAIN_APPROX_NONE)
 
     for contour in contours:
-        if cv2.contourArea(contour) > 100:
+        if 5000 > cv2.contourArea(contour) > 100:
             (x, y, w, h) = cv2.boundingRect(contour)
             cv2.rectangle(copy, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-
-
+            print(x, y, w, h)
+            cv2.imshow('Automatic HSV', copy)
+            cv2.waitKey(0)
 
 def video_lecture(video_name):
 
@@ -69,7 +74,7 @@ def video_lecture(video_name):
         elapsed_time = time.time() - start_time
         print(elapsed_time)
 
-        if cv2.waitKey(400) & 0xFF == ord('q'):
+        if cv2.waitKey(0) & 0xFF == ord('q'):
             cv2.imwrite('ici.jpg', face_cap)
             break
 
