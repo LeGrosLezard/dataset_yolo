76f230f2639020d9aa8654102eba31ef55c12980
LeGrosLezard <53193299+LeGrosLezard@users.noreply.github.com>
2019-11-30 02:22:38 +0100

    Update find_hand.py

--- code/find_hand.py
+++ code/find_hand.py
@@ -4,31 +4,32 @@ import time
 import dlib
 
 
-def find_head(face_cap, out, detector):
+def find_head(face_cap, out, detector, frame1, frame2):
 
-    faces = detector(cv2.cvtColor(face_cap, cv2.COLOR_BGR2GRAY))
-    for face in faces:
-        x1 = face.left()
-        y1 = face.top()
-        x2 = face.right()
-        y2 = face.bottom()
-        cv2.rectangle(out, (x1, y1), (x2, y2), (0, 0, 255), 1)
-
-
-    return (x1, y1, x2, y2)
-
-
-
-def delete_area_face(head, frame1, frame2):
+    #Make difference of background
     diff = cv2.absdiff(frame1, frame2)
 
+    #Collect points of face
 
-def find_hand(frame1, frame2, copy):
 
-    diff = cv2.absdiff(frame1, frame2)
+
+    contours = [np.array([ [face.left() - 10, face.top() - 50],
+                [face.right() + 10, face.top() - 50],
+                [face.right() + 10, face.bottom() + 10],
+                [face.left() - 10, face.bottom() + 10] ])
+                for face in
+                detector(cv2.cvtColor(face_cap, cv2.COLOR_BGR2GRAY))]
+
+    cv2.fillPoly(diff, pts=[contours][0], color=(255,255,255))
+
+    return diff
+
+
+
+def find_hand(copy, diff):
 
     mask = cv2.inRange(cv2.cvtColor(diff, cv2.COLOR_BGR2HSV),
-                        np.array([0, 32, 10]),
+                        np.array([0, 32, 30]),
                         np.array([255, 255, 255]))
 
     contours, _ = cv2.findContours(mask, cv2.RETR_TREE,
@@ -57,14 +58,13 @@ def video_lecture(video_name):
         face_cap = cv2.resize(video.read()[1], (800, 500))
         copy = frame1.copy()
 
-        #try: head = find_head(face_cap, face_cap, detector)
-        #except: pass
+ 
+        diff = find_head(face_cap, copy, detector, frame1, frame2)
 
-        find_hand(frame1, frame2, copy)
+        find_hand(copy, diff)
 
-
-        #cv2.imshow('face_cap', face_cap)
         cv2.imshow('Automatic HSV', copy)
+
 
 
         frame1 = frame2
@@ -73,7 +73,7 @@ def video_lecture(video_name):
         elapsed_time = time.time() - start_time
         print(elapsed_time)
 
-        if cv2.waitKey(0) & 0xFF == ord('q'): break
+        if cv2.waitKey(2) & 0xFF == ord('q'): break
 
     video.release()
     cv2.destroyAllWindows()
